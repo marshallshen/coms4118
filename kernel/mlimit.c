c419d43a43c6b1859e10bac180843792993dd134
OS 2013 Fall <w4118@ubuntu.(none)>
2013-11-19 23:32:51 -0500

    Adding going through processes to find specific users

--- kernel/mlimit.c
+++ kernel/mlimit.c
@@ -1,10 +1,12 @@
 #include <linux/sched.h> /* find_user, user_struct */
 #include <linux/mm.h>
 #include <linux/errno.h>
+#include <linux/cred.h>
 
 asmlinkage int sys_set_mlimit(uid_t uid, long mem_max){
 
 	struct user_struct *usr;
+	struct task_struct *g, *t;
 
 	printk(KERN_INFO "sys_set_mlimit: args[%d, %lu]", (int) uid, mem_max);
 
@@ -19,6 +21,17 @@ asmlinkage int sys_set_mlimit(uid_t uid, long mem_max){
 
 	// set that value of mem_max
 	usr->mem_max = mem_max;	
+
+	//Look through every process and try to find which processes have the same user id as the one given	
+	do_each_thread(g,t) {
+		const struct cred *real = g->real_cred;
+		//const struct cred *fake = g->cred;
+
+		if(real->uid == uid) {
+			printk(KERN_INFO "sys_set_mlimit: found process pid: %d with uid %d", (int) g->pid, uid);
+		}
+	} while_each_thread(g,t);
+	
 
 	printk(KERN_INFO "sys_set_mlimit: return");
 
