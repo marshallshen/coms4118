f25c6e8f34354e14d751b3a24354997134673cf2
Vanshil <vs2409@columbia.edu>
2013-12-04 01:29:24 -0500

    Tests 1 and 2 for the src are working. Test 3 still needs work. DON'T CHANGE MY CODE

--- fs/ext4/cowcopy.c
+++ fs/ext4/cowcopy.c
@@ -9,42 +9,55 @@
 asmlinkage int sys_ext4_cowcopy(const char __user *src, const char __user *dest){
 	
 	//Checking if the path is a directory of not.
+
+	//For the src
 	struct path s_p;
 	struct dentry *s_d;
 	struct vfsmount *s_mnt;
 	struct inode *s_i;
 	struct super_block *s_sb;
 	struct file_system_type *s_type;
-	char *s_fsName;
+	const char *s_fsName;
+
+	//for the destination
 	struct path d_p;
 	struct dentry *d_d;
 	struct vfsmount *d_mnt;
 	struct inode *d_i;
 
-	//Might need to sanitize the src filename and check if its like a relative or absolute path.
-	//Another thing to note is that for kern_path, it says it takes a char *name but we're giving it a path so idk if this will work
 	kern_path(src, LOOKUP_FOLLOW, &s_p);
 	s_d = s_p.dentry;
 	s_mnt = s_p.mnt;
-	s_i = s_d->d_inode;
-	s_sb = s_i->i_sb;
-	s_type = s_sb->s_type;
-	s_fsName = s_type->name;
+	if(s_d) {
+		s_i = s_d->d_inode;
+		s_sb = s_i->i_sb;
+		s_type = s_sb->s_type;
+		s_fsName = s_type->name;
+		printk(KERN_INFO "COWS: fsName: %s\n", s_fsName);
+	}
+	printk(KERN_INFO "COWS: src: %s\n", src);
+	printk(KERN_INFO "COWS: dest: %s\n", dest);
 	
 	kern_path(dest, LOOKUP_FOLLOW, &d_p);
 	d_d = d_p.dentry;
 	d_mnt = d_p.mnt;
-	d_i = d_d->d_inode;
+	if(d_d) {
+		d_i = d_d->d_inode;
+	}
 
 	//Checking if the file is actually a file and not a directory
-	if(S_ISDIR(i->i_mode))
+	if(S_ISDIR(s_i->i_mode)) {
+		printk(KERN_INFO "COWS: directory bitch\n");
 		return -EPERM;
+	}
 
 	//Checking if the file is in the ext4 file system
-	if(!strncmp(fsName, "ext4", 4)) 
+	if(!strncmp(s_fsName, "ext4", 4)) {
+		printk(KERN_INFO "COWS: not ext4 bitch\n");
 		return -EOPNOTSUPP;
+	}
 	
-
+	
 	
 	printk(KERN_INFO "sys_ext4_cowcopy: success");
 	return 0;
